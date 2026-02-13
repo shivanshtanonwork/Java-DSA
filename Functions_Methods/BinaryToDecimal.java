15116f2489ff9e80b65d97c4507db3e0ec4993ef
Shivansh Tandon <shivanshtandonwork@gmail.com>
2026-02-13 10:24:37 +0530

    Binary to decimal and Decimal to binary

---
+++ Functions_Methods/BinaryToDecimal.java
@@ -0,0 +1,34 @@
+
+public class BinaryToDecimal {
+    public static void binToDec(int binNum){
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
+
+        while(binNum > 0){
+            int lastDigit = binNum % 10;
+            decNum = decNum + lastDigit * (int)Math.pow(2, pow);
+            pow++;
+            binNum = binNum / 10;
+        }
+        System.out.println("decimal of " + myNum + " = "+ decNum);
+    }
+
+    public static void decToBin(int n){
+        int myNum = n;
+        int pow = 0;
+        int binNum = 0;
+        while(n>0){
+            int rem = n % 2;
+            binNum = binNum + rem * (int)Math.pow(10, pow);
+            pow++;
+            n=n/2;
+        }
+        System.out.println("Binary form of " + myNum + " = " + binNum);
+    }
+    public static void main(String[] args) {
+        // binToDec(101);
+        decToBin(5);
+    }
+
+}
--- Functions_Methods/primeOrNot.java
+++ Functions_Methods/primeOrNot.java
@@ -13,7 +13,7 @@ public class primeOrNot {
     public static void primeInRange(int n){
         for(int i = 2; i<=n;i++){
             if(isPrime(i)){
-                System.out.print(i+" ");
+                System.out.print(i+" ");    
             }
         }
         System.out.println();
